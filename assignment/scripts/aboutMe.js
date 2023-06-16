a19a544371ef5af46c5efa2e82a3e2b1279f945d
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-06-16 18:09:34 -0500

    cleaned up t + f text as not a compound conditional, duh

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -28,10 +28,10 @@ pets = pets + 2;
 let allowedPets = 6;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-if (adventurous === true){
-    console.log('Adventures are great!');}
-else if (adventurous === false){
-    console.log('How about we stay home?');}
+if (adventurous)
+{console.log('Adventures are great!');} // yay!!!
+else 
+{console.log('How about we stay home?');} // nayyy :(
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
