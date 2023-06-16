47a3c2d022e476e49c51756ed315a40679049b8a
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-06-16 16:07:24 -0500

    lastName addition

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,8 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-const firstName = 'Hannah';
+let firstName = 'Hannah';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Comparetto';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 
